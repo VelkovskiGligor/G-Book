72d31432f43837e5d572d09c14c651e0fe407c55
velko <velkovskigligor@gmail.com>
2022-06-09 15:05:57 +0200

    initial commit

--- src/main/java/mk/ukim/finki/gbook/models/exception/BookRatingNotFoundException.java
+++ src/main/java/mk/ukim/finki/gbook/models/exception/BookRatingNotFoundException.java
@@ -1,5 +1,10 @@
 package mk.ukim.finki.gbook.models.exception;
 
+import org.aspectj.weaver.ast.Not;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class BookRatingNotFoundException extends RuntimeException{
     public BookRatingNotFoundException(String username,Long id) {
         super( String.format("Book rating not found for User with username %s and Book with id %d",username,id));
